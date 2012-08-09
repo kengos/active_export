81dbd04a24f05af5571844d0257643cb68255659
kengos <kengo@kengos.jp>
2012-08-09 23:21:31 +0900

    set target

--- spec/active_export/base_spec.rb
+++ spec/active_export/base_spec.rb
@@ -6,14 +6,13 @@ describe ActiveExport::Base do
   let(:author_1) { Author.create!(name: 'author_1') }
   let(:author_2) { Author.create!(name: 'author_2') }
 
-
   describe ".export" do
     let!(:book_1) { Book.create!(name: 'book_1', author: author_1, price: 1000) }
     let!(:book_2) { Book.create!(name: 'book_2', author: author_2, price: 2000) }
 
     let(:export_methods) { %w(name author.name price) }
     subject { ActiveExport::Base.export(Book.order('id ASC').all, export_methods) }
-    its(:first) { %w(book_1 author_1 1000) }
-    its(:last) { %w(book_2 author_2 2000) }
+    its(:first) { should eql %w(book_1 author_1 1000) }
+    its(:last) { should eql  %w(book_2 author_2 2000) }
   end
 end
