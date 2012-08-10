52484f9058017026b87c8a59af7294faec7503ab
kengos <kengo@kengos.jp>
2012-08-10 23:23:53 +0900

    add csv_spec and rm code

--- lib/active_export/csv.rb
+++ lib/active_export/csv.rb
@@ -1,22 +1,14 @@
 # coding: utf-8
 
 module ActiveExport
-  class Csv
+  class Csv < Base
     class << self
       # @param [Array or ActiveRecord::Relation] data
       # @param [Hash]
       # @exmaple
       #   AcriveExport::Csv.export(data, :source => :namespace)
       def export(data, settings, options = {})
-        _optoins = { :header => true }.merge(options)
-        return '' if data.size == 0
-        header_keys = data.first.attributes.keys
-        csv_array = []
-        each_method_name = data.respond_to?(:find_each) ? 'find_each' : 'each'
-        data.send(each_method_name) do |row|
-          csv_array << row.attributes.values
-        end
-        csv_array
+
       end
 
       def setting_parser(settings)
---
+++ spec/active_export/csv_spec.rb
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+require 'spec_helper'
+
+describe ActiveExport::Csv do
+  
+end
