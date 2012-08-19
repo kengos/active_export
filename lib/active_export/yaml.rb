1fe642c1b1ee691c8d66895cbe7fd19e54f9c0e4
kengos <kengo@kengos.jp>
2012-08-19 23:29:51 +0900

    add xml and yaml exporter

---
+++ lib/active_export/yaml.rb
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+module ActiveExport
+  class Yaml < ::ActiveExport::Base
+    def self.export(data, source, namespace, options = {})
+      new(source, namespace, options).export(data)
+    end
+
+    def self.export_file(data, source, namespace, filename, options = {})
+      new(source, namespace, options).export(data, filename)
+    end
+
+    def export(data)
+    end
+
+    def export_file(data, filename)
+    end
+  end
+end
