e1b5d4b971951be6889dadf5e7b584625021cdf7
kengos <kengo@kengos.jp>
2012-08-21 00:27:14 +0900

    add memo

--- lib/active_export/xml.rb
+++ lib/active_export/xml.rb
@@ -2,6 +2,28 @@
 
 module ActiveExport
   class Xml < ::ActiveExport::Base
+    # @return [String] XML style string
+    # @memo yaml format
+    # - namespace:
+    #   label_prefix: 'book'
+    #   methods:
+    #     - name
+    #     - author.name
+    #     - price: price * 1.095
+    #    xml_format:
+    #      encoding: 'UTF8'
+    #      header: |
+    #<?xml version="1.0" encoding="UTF-8"?>
+    #<records>
+    #      footer: |
+    #</records>
+    #      body: |
+    #  <book>
+    #    <name>%%name%%</name>
+    #    <author_name>%%author_name%%</author_name>
+    #    <price>%%price%%</price>
+    #  </book>
+    #  
     def export(data)
       # TODO
     end
